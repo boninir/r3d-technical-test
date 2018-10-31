0dd5299951c60eabff4ff40ac97bc70ff8e970df
Bonini Raphael <raphael.eros.bonini@gmail.com>
2018-10-31 09:30:12 -0400

    implement the database and features

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,7 +2,10 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Product;
+use AppBundle\Form\ProductType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Swift_Message;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -13,9 +16,36 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', array(
-            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
-        ));
+        $product = new Product();
+        $form = $this->createForm(ProductType::class, $product);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($product);
+            $entityManager->flush();
+
+            $this->sendEmail('raphael.eros.bonini@gmail.com', $product);
+
+            return $this->redirect($this->generateUrl('homepage'));
+
+        }
+
+        return $this->render(
+            'default/index.html.twig',
+            array('form' => $form->createView())
+        );
+    }
+
+    private function sendEmail(string $email, Product $product)
+    {
+        $message = (new Swift_Message('New Product Created'))
+            ->setFrom('no-reply@example.com')
+            ->setTo($email)
+            ->setBody(\sprintf('the product %s was correctly created.', $product->getName()))
+        ;
+
+        $this->get('mailer')->send($message);
     }
 }
