378cfc974a5aebc9599c6dfe93b12888ac043015
Bonini Raphael <raphael.eros.bonini@gmail.com>
2018-11-06 11:39:04 -0500

    add some front stuff to the application

--- src/AppBundle/Form/ProductType.php
+++ src/AppBundle/Form/ProductType.php
@@ -3,7 +3,10 @@
 namespace AppBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -15,10 +18,10 @@ class ProductType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('description')
-            ->add('price')
-            ->add('save', SubmitType::class, array('label' => 'Create Post'));
+            ->add('name', TextType::class, array('label' => 'Nom : ', 'attr' => array('class' => 'form-control')))
+            ->add('description', TextareaType::class, array('label' => 'Description : ', 'attr' => array('class' => 'form-control')))
+            ->add('price', IntegerType::class, array('label' => 'Prix : ', 'attr' => array('class' => 'form-control')))
+            ->add('save', SubmitType::class, array('label' => 'Ajouter un produit', 'attr' => array('class' => 'btn btn-primary')));
     }
     
     /**
